b305212c60b46b67fd189af88852f99cebc47f63
rudyon <keremberket@gmail.com>
2023-12-19 00:48:48 +0300

    simple shooty broken state

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 var (
-	player Player = Player{300, 400, 2}
-	solids []Solid
+	player  Player = Player{300, 400, 2}
+	solids  []Solid
+	bullets []Bullet
 )
 
 type Player struct {
@@ -16,7 +19,13 @@ type Solid struct {
 	collider rl.Rectangle
 }
 
-func player_colliding() bool {
+type Bullet struct {
+	x, y  float32
+	speed float32
+	angle float32
+}
+
+func colliding_player_solid() bool {
 	for i := 0; i < len(solids); i++ {
 		if rl.CheckCollisionRecs(rl.NewRectangle(player.x-18, player.y-18, 18*2, 18*2), solids[i].collider) {
 			return true
@@ -26,7 +35,7 @@ func player_colliding() bool {
 	return false
 }
 
-func player_update() {
+func update_player() {
 	// TODO: the code inside of here causes it so that moving diagonal is double the speed
 	// i don't want this. i don't think. maybe i can leave it still.
 	// for later consideration the collision took too long
@@ -35,7 +44,7 @@ func player_update() {
 	if rl.IsKeyDown(rl.KeyD) {
 		player.x += player.speed
 
-		for player_colliding() {
+		for colliding_player_solid() {
 			player.x--
 		}
 	}
@@ -43,7 +52,7 @@ func player_update() {
 	if rl.IsKeyDown(rl.KeyA) {
 		player.x -= player.speed
 
-		for player_colliding() {
+		for colliding_player_solid() {
 			player.x++
 		}
 	}
@@ -51,7 +60,7 @@ func player_update() {
 	if rl.IsKeyDown(rl.KeyW) {
 		player.y -= player.speed
 
-		for player_colliding() {
+		for colliding_player_solid() {
 			player.y++
 		}
 	}
@@ -59,9 +68,13 @@ func player_update() {
 	if rl.IsKeyDown(rl.KeyS) {
 		player.y += player.speed
 
-		for player_colliding() {
+		for colliding_player_solid() {
 			player.y--
 		}
+	}
+
+	if rl.IsMouseButtonDown(rl.MouseButtonLeft) {
+		bullets = append(bullets, Bullet{player.x, player.y, 4, 4})
 	}
 }
 
@@ -71,8 +84,22 @@ func draw_obstacles() {
 	}
 }
 
+func draw_bullets() {
+	for i := 0; i < len(bullets); i++ {
+		rl.DrawCircle(int32(bullets[i].x), int32(bullets[i].y), 16, rl.Green)
+	}
+}
+
 func update() {
-	player_update()
+	update_player()
+	update_bullet()
+}
+
+func update_bullet() {
+	for i := 0; i < len(bullets); i++ {
+		bullets[i].x += bullets[i].speed
+		bullets[i].y += bullets[i].speed
+	}
 }
 
 func draw() {
@@ -81,6 +108,7 @@ func draw() {
 
 	rl.DrawRectangleRec(rl.NewRectangle(player.x-18, player.y-18, 18*2, 18*2), rl.Red)
 	draw_obstacles()
+	draw_bullets()
 
 	rl.EndDrawing()
 }
